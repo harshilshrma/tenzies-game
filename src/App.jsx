2a89953c579c2b0cd1216871c833f25ea14e42f3
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-12-09 22:44:36 +0530

    mobile opt + roll die button + die as obj

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import Die from './components/Die.jsx'
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: false
+      }))
+  }
+
+  function handleDiceRoll() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map((dieObj, index) => {
+    return <Die key={index} value={dieObj.value} />
+  })
 
   return (
     <main>
-      
+      <div className="die-container">
+        {diceElements}
+      </div>
+      <button className="roll-die" onClick={handleDiceRoll}>Roll</button>
     </main>
   )
 }
+
 
 export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+import './Die.css'
+
+export default function Die(props) {
+    return (
+        <button>
+            {props.value}
+        </button>
+    )
+}
